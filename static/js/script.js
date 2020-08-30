88a0571128fae9c0f33d42139aceb20417e0acec
dsobacari <dsobacari@gmail.com>
2020-08-30 11:48:21 -0400

    modified:   index.html 	modified:   static/css/style.css 	modified:   static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -100,13 +100,60 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 }
 
+//Challange 4: Change the Color of All Buttons
+var all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons);
+
+var copyAllButtons = [];
+for (let i=0; i < all_buttons.length; i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
 
 
 
+function buttonColorChange(buttonThingy) {
+    if (buttonThingy.value === 'red') {
+        buttonsRed();
+    } else if (buttonThingy.value === 'green') {
+        buttonsGreen();
+    } else if (buttonThingy.value ==='reset') {
+        buttonsReset();
+    } else if (buttonThingy.value ==='random') {
+        randomColors();
+    }
+}
+
+function buttonsRed(){
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonsGreen(){
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
 
 
+function buttonsReset(){
+    for (let i=0; i < all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]); //replace with i value from our copy
+    }
+}
 
-
+function randomColors(){
+    let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'];
+    
+    for (let i=0; i < all_buttons.length; i++) {    
+        let randomNumber = Math.floor(Math.random() * 4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]); 
+    }
+}
 
 
 
